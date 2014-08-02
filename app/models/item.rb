f011fd9568b1850c17533c947fc3d54c601f6bff
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-02 03:08:59 -0700

    Impement #Post

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,13 +5,14 @@ class Item < ActiveRecord::Base
     params = { endpoint: endpoint }
     puts response = get_to_endpoint(params).body
     response_json = JSON.parse(response, symbolize_names: true)[:response][:data]
-    puts "\n\n Product Order"
+    puts "\n\n Product List"
     product_order = response_json[:list].to_json
   end
 
-  def self.destroy_items(id)
+  def self.destroy_items(delete_params)
     endpoint = 'https://hiku-staging.herokuapp.com/api/v1/list'
-    params = { endpoint: endpoint, id: id }
+    delete_params = delete_params.reject { |k, _| k == "action" || k == "controller" }
+    params = { endpoint: endpoint }.merge(delete_params).symbolize_keys
     puts response = delete_to_endpoint(params).body
     response_json = JSON.parse(response, symbolize_names: true)[:response][:reponse]
   end
@@ -19,6 +20,13 @@ class Item < ActiveRecord::Base
   private
   def self.get_to_endpoint(params)
     HikuConnect::Get.new({
+      endpoint: params[:endpoint],
+      token: '9604d9a375315cb55d471cf11ff31584'
+    }).response
+  end
+
+  def self.post_to_endpoint(params)
+    HikuConnect::Post.new({
       endpoint: params[:endpoint],
       token: '9604d9a375315cb55d471cf11ff31584'
     }).response
