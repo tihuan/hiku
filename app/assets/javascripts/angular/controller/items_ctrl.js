1a79f05c9d8aa4f1392b7dbd566dd7587a7f9e8a
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-03 08:43:57 -0700

    add item id after success callback

--- app/assets/javascripts/angular/controller/items_ctrl.js
+++ app/assets/javascripts/angular/controller/items_ctrl.js
@@ -2,11 +2,19 @@ app.controller('ItemsController', ['Item', function(Item) {
   this.items = Item.all();
 
   this.createItem = function() {
+    var itemsCtrl = this;
     var attr = {};
     attr.name = this.name;
     attr.quantity = this.quantity;
     var newItem = Item.create(attr);
-    this.items.push(newItem);
+    itemsCtrl.items.push(newItem);
+    newItem.$promise.then(
+      function(success) {
+        var all_items = itemsCtrl.items;
+        var item_created = all_items[all_items.length-1];
+        item_created['id'] = item_created.data.id;
+      }
+      )
     this.name = "";
     this.quantity = "";
   };
