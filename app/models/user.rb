11d7a6bd71c51ccd31f4ec177d9f3a4187e5e0af
Timmy Huang <timmyhuang0404@gmail.com>
2014-08-12 15:06:54 -0700

    fix HikuConnect::Post to HikuConnect.new.post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     endpoint = 'https://hiku-staging.herokuapp.com/api/v1/login'
     login_params = login_params.reject { |k, _| k == "action" || k == "session" }
     delete_params = { endpoint: endpoint }.merge(login_params).symbolize_keys
-    puts response = HikuConnect::Post.new(delete_params).response.body
+    puts response = HikuConnect.new(delete_params).post.response.body
     response_json = JSON.parse(response, symbolize_names: true)[:response]
   end
 end
